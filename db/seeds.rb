5880fbc3e37983439b71ba7d73c0ee001eb3e8bb
Vanelfokamcode <Vanelfokam2003@gmail.com>
2023-04-22 09:29:31 +0200

    add active admin panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# AdminUser.create!(email: 'va@gmail.com', password: 'test123', password_confirmation: 'test123')
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
