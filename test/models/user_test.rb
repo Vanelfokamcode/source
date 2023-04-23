99348ca29038f2dbca588feb87ede1c45c3d0732
Vanelfokamcode <Vanelfokam2003@gmail.com>
2023-04-23 13:23:42 +0200

    added simple test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save if user is blank" do
+    assert_equal User.new.save, false
+  end
+
+  test "should if user is full" do
+    # users name of users.yml
+
+    @user = users(:va)
+    assert_equal @user.save, true
+  end
 end
