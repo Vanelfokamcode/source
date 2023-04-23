1b890c262746898238d00ac42f45c52ca2523ee2
Vanelfokamcode <Vanelfokam2003@gmail.com>
2023-04-23 11:06:00 +0200

    added categories to items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
+    has_many :category_items
+
+    has_many :categories, through: :category_items
     has_one_attached :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_23_061720) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_23_065640) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -71,6 +71,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_23_061720) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "category_items", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_items_on_category_id"
+    t.index ["item_id"], name: "index_category_items_on_item_id"
+  end
+
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -90,4 +99,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_23_061720) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "category_items", "categories"
+  add_foreign_key "category_items", "items"
 end
