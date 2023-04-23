1d9c6752f56815d22470aac8040c2799ff6f836a
Vanelfokamcode <Vanelfokam2003@gmail.com>
2023-04-23 08:47:05 +0200

    add category model and controller setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_22_074549) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_23_061720) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -63,6 +63,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_22_074549) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
